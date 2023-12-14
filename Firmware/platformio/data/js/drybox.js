fdace56f0deb797ea3c52eb67236a1f61c03b59d
Sanugli <sanugli@onyx.ilgunas.lan>
2023-12-13 17:34:33 -0800

    Updated some of the html

--- Firmware/platformio/data/js/drybox.js
+++ Firmware/platformio/data/js/drybox.js
@@ -1,144 +1,138 @@
 // Shorthand for $( document ).ready()
 $(function() {
 
-	$('#sliderTemperature').on('input change', function(){	
-			$("label[for='labelSetTemperature']").text("Target temperature: " + parseInt(this.value) +"°C");
-		});
+  $('#sliderTemperature').on('input change', function(){
+      $("label[for='labelSetTemperature']").text(parseInt(this.value) +"°C");
+    });
 
-	$('#sliderHeaterTemperature').on('input change', function(){	
-			$("label[for='labelSetHeaterTemperature']").text("Max heater temperature: " + parseInt(this.value) +"°C");
-		});
+  $('#sliderHeaterTemperature').on('input change', function(){
+      $("label[for='labelSetHeaterTemperature']").text( parseInt(this.value) +"°C");
+    });
 
-	$('#sliderFanSpeed').on('input change', function(){	
-			$("label[for='labelSetFanSpeed']").text("Fan speed: " + parseInt(this.value) +"%");
-		});
+  $('#sliderFanSpeed').on('input change', function(){
+      $("label[for='labelSetFanSpeed']").text(parseInt(this.value) +"%");
+    });
 
-	$('#btnSet').on('click', function(){
-		var temperature = $("#sliderTemperature").val();
-		var heater = $("#sliderHeaterTemperature").val();
-		var fanspeed = $("#sliderFanSpeed").val();
+  $('#btnSet').on('click', function(){
+    var temperature = $("#sliderTemperature").val();
+    var heater      = $("#sliderHeaterTemperature").val();
+    var fanspeed    = $("#sliderFanSpeed").val();
+    console.log("Set command sent");
 
-		// Send 
-		$.ajax({
-		  url: "/set",
-		  type: "get", //send it through get method
-		  data: { 
-		    temperature: temperature, 
-		    heater: heater, 
-		    fanspeed: fanspeed,
-		  },
-		  success: function(response) {
-		    //Do Something
-		    console.log(response);
-		    update_status(false);
-		  },
-		  error: function(xhr) {
-		    //Do Something to handle error
-		  }
-		});
-	});
+    // Send
+    $.ajax({
+      url: "/set",
+      type: "get", //send it through get method
+      data: {
+        temperature : temperature,
+        heater      : heater,
+        fanspeed    : fanspeed,
+      },
+      success: function(response) {
+        //Do Something
+        console.log(response);
+        update_status(false);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+        console.log( xhr );
+      }
+    });
+  });
 
-	$('#btnTurnOff').on('click', function(){
-		// Send 
-		$.ajax({
-		  url: "/off",
-		  type: "get", //send it through get method
-		  success: function(response) {
-		    console.log(response);
-		    update_status(false);
-		  },
-		  error: function(xhr) {
-		    //Do Something to handle error
-		  }
-		});
-	});
+  $('#btnTurnOff').on('click', function(){
+    console.log("Turn off command sent");
+    // Send
+    $.ajax({
+      url: "/off",
+      type: "get", //send it through get method
+      success: function(response) {
+        console.log(response);
+        update_status(false);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+        console.log( xhr );
+      }
+    });
+  });
+  $('#btnReboot').on('click', function(){
+      console.log("Reboot command sent");
 
-	$('a#pop-temp').popover({
-			    trigger : 'click',  
-			    placement : 'top', 
-			    html: true, 
-			    title : 'Target Temperature',
-			    content : 'This is the maximum/desired temperature we want inside the box.<br>\
-			    			The system will try to reach and then maintain this temperature.',
-            });
+    // Send
+    $.ajax({
+      url: "/reboot",
+      type: "get", //send it through get method
+      data: { },
+      success: function(response) {
+        //Do Something
+        console.log("Response received");
+        console.log(response);
+        update_status(false);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+        console.log( xhr );
+      }
+    });
+  });
 
-
-	$('a#pop-fan').popover({
-			    trigger : 'click',  
-			    placement : 'top', 
-			    html: true, 
-			    title : 'Fan speed',
-			    content : 'Set the speed of the internal fan used to circulate the air inside the dry box.<br><br>\
-			    			0% = Off<br>\
-			    			50% = 50% of fan\'s maximum RPM<br>\
-			    			100% = Full speed',
-            });
-
-
-	$('a#pop-heater').popover({
-			    trigger : 'click',  
-			    placement : 'top', 
-			    html: true, 
-			    title : 'Max heater temperature',
-			    content : 'Maximum allowed temerature heater can reach.<br>This limit should be set to avoid damage to your filament or the box/heater itself.<br>\
-			    			For example; Let\'s say that your heater could reach max temperature of 150°C. However this could start to melt your filament, melt the dry box or start a fire.<br>\
-			    			In order to avoid this, we set a heater temperature limit to make sure heater never passes this set temperature during normal operation.<br>\
-			    			This limit will also affect how long it takes to reach target temperature.',
-            });
-	// Finally update status
-	update_status(true);
+  // Finally update status
+  update_status(true);
 });
 
 
-function update_status(rearm)
-{
-	var ajaxTime= new Date().getTime();
+function update_status(rearm) {
+  var ajaxTime= new Date().getTime();
 
-	$.ajax({
-	url: "/status",
-	type: "get", //send it through get method
-	success: function(response) {
-		console.log(response);
+  $.ajax({
+  url: "/status",
+  type: "get", //send it through get method
+  success: function(response) {
+    console.log(response);
 
-		var res = jQuery.parseJSON(response);
+    var res = jQuery.parseJSON(response);
 
-		$('#status-temp-target').text(res.target_temp_in);
-		$('#status-fan-speed').text(res.fan_speed);
-		$('#status-temp-inside').text(res.temp_in.toFixed(1));
-		$('#status-temp-inside2').text(res.temp_in.toFixed(1));
-		$('#status-temp-inside-target').text(res.target_temp_in.toFixed(1));
-		$('#status-temp-heater').text(res.temp_heater.toFixed(0));
-		$('#status-temp-heater-max').text(res.max_temp_heater.toFixed(0));
-		$('#status-humidity-inside').text(res.humid_in.toFixed(0));
-		$('#status-temp-outside').text(res.temp_out.toFixed(0));
-		$('#status-humidity-outside').text(res.humid_out.toFixed(0));
+    $('#status-temp-target').text(res.target_temp_in);
+    $('#status-fan-speed').text(res.fan_speed);
+    $('#status-temp-inside').text(res.temp_in.toFixed(2));
+    $('#status-temp-inside2').text(res.temp_in.toFixed(2));
+    $('#status-temp-inside-target').text(res.target_temp_in.toFixed(2));
+    $('#status-temp-heater').text(res.temp_heater.toFixed(2));
+    $('#status-temp-heater-max').text(res.max_temp_heater.toFixed(2));
+    $('#status-humidity-inside').text(res.humid_in.toFixed(2));
+    $('#status-temp-outside').text(res.temp_out.toFixed(2));
+    $('#status-humidity-outside').text(res.humid_out.toFixed(2));
 
-		// Homed status
-		if(res.status == 1) {
-			$('#status-box').text('Dry box is on').addClass('badge-success').removeClass('badge-warning').removeClass('badge-danger');
-		}
-		else {
-			$('#status-box').text('Dry box is off').addClass('badge-warning').removeClass('badge-success').removeClass('badge-danger');
-		}
+    // Homed status
+    if(res.status == 1) {
+      $('#status-box').text('Heater is on').addClass('badge-danger').removeClass('badge-secondary').removeClass('badge-success');
+      $('#btnSet').prop('disabled', false);
+      $('#btnSet').removeClass('disabled');
+    }
+    else {
+      $('#status-box').text('Heater is off').addClass('badge-success').removeClass('badge-secondary').removeClass('badge-danger');
+      $('#btnSet').addClass('disabled');
+    }
 
-	},
-	error: function(xhr) {
-		$('#status-box').text('Dry box is unreachable!').addClass('badge-danger').removeClass('badge-success').removeClass('badge-warning');
-	}
-	}).done(function () {
+  },
+  error: function(xhr) {
+    $('#status-box').text('Dry box is unreachable').addClass('badge-secondary').removeClass('badge-success').removeClass('badge-danger');
+  }
+  }).done(function () {
     var totalTime = new Date().getTime()-ajaxTime;
+    console.log( "totalTime: " + totalTime );
     // Here I want to get the how long it took to load some.php and use it further
-	});;
+  });;
 
-	if(rearm) {
-		rearm_status();
-	}
+  if(rearm) {
+    rearm_status();
+  }
 }
 
-function rearm_status()
-{
-	setTimeout(function () {
-		update_status(true);
-	}, 2000);
+function rearm_status() {
+  setTimeout(function () {
+    update_status(true);
+  }, 2000);
 }
 
